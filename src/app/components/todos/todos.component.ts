6f4cf24a6c5888f561a593a8244fb575228f2c08
Breyanna Vass <breyannavass@gmail.com>
2019-09-05 17:50:01 -0400

    fixing function name errors

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -24,10 +24,17 @@ export class TodosComponent implements OnInit {
     });
   }
 
-  onDelete(todo: Todo) {
-    // UI
+  deleteTodo(todo: Todo) {
+    // Remove From UI
     this.todos = this.todos.filter(t => t.id !== todo.id);
-    // Server
+    // Remove from server
     this.todoService.deleteTodo(todo).subscribe();
+  }
+
+  addTodo(todo: Todo) {
+    // tslint:disable-next-line: no-shadowed-variable
+    this.todoService.addTodo(todo).subscribe(todo => {
+      this.todos.push(todo);
+    });
   }
 }
