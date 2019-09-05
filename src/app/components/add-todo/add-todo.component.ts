111ab08c2c17e4033d7bdf14b37ef450631b618b
Breyanna Vass <breyannavass@gmail.com>
2019-09-05 17:40:43 -0400

    updating add todo component

--- src/app/components/add-todo/add-todo.component.ts
+++ src/app/components/add-todo/add-todo.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from "@angular/core";
 
 @Component({
-  selector: 'app-add-todo',
-  templateUrl: './add-todo.component.html',
-  styleUrls: ['./add-todo.component.css']
+  selector: "app-add-todo",
+  templateUrl: "./add-todo.component.html",
+  styleUrls: ["./add-todo.component.css"]
 })
 export class AddTodoComponent implements OnInit {
+  @Output() addTodo: EventEmitter<any> = new EventEmitter();
 
-  constructor() { }
+  title: string;
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  onSubmit() {
+    const todo = {
+      title: this.title,
+      completed: false
+    };
+
+    this.addTodo.emit(todo);
   }
-
 }
