f869dc1817573418790361b9c7fa6b0f9e5c264a
Breyanna Vass <breyannavass@gmail.com>
2019-09-05 17:01:28 -0400

    update service and todo item

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,34 +1,47 @@
+// tslint:disable-next-line: quotemark
 import { Component, OnInit, Input } from "@angular/core";
+// tslint:disable-next-line: quotemark
 import { Todo } from "src/app/models/Todo";
+// tslint:disable-next-line: quotemark
+import { TodoService } from "../../services/todo.service";
 
 @Component({
+  // tslint:disable-next-line: quotemark
   selector: "app-todo-item",
+  // tslint:disable-next-line: quotemark
   templateUrl: "./todo-item.component.html",
+  // tslint:disable-next-line: quotemark
   styleUrls: ["./todo-item.component.css"]
 })
 export class TodoItemComponent implements OnInit {
   @Input() todo: Todo;
 
-  constructor() {}
+  constructor(private todoService: TodoService) {}
 
   ngOnInit() {}
 
   // Set Classes
   setClasses() {
-    let classes = {
+    const classes = {
       todo: true,
+      // tslint:disable-next-line: quotemark
       "is-complete": this.todo.completed
     };
 
     return classes;
   }
 
-  onToggle(todo) {
-    console.log("toggle");
+  onToggle(todo: Todo) {
+    // Toggle in UI
     todo.completed = !todo.completed;
+    // Toggle on server
+
+    // tslint:disable-next-line: no-shadowed-variable
+    this.todoService.toggleCompleted(todo).subscribe(todo => console.log(todo));
   }
 
   onDelete(todo) {
+    // tslint:disable-next-line: quotemark
     console.log("delete");
   }
 }
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -8,17 +8,33 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 // tslint:disable-next-line: quotemark
 import { Todo } from "../models/Todo";
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    // tslint:disable-next-line: quotemark
+    "Content-Type": "application/json"
+  })
+};
+
 @Injectable({
   // tslint:disable-next-line: quotemark
   providedIn: "root"
 })
 export class TodoService {
   // tslint:disable-next-line: quotemark
-  todosUrl = "https://jsonplaceholder.typicode.com/todos?_limit=5";
+  todosUrl = "https://jsonplaceholder.typicode.com/todos";
+  // tslint:disable-next-line: quotemark
+  todosLimit = "?_limit=10";
 
   constructor(private http: HttpClient) {}
 
+  // Get Todod
   getTodos(): Observable<Todo[]> {
-    return this.http.get<Todo[]>(this.todosUrl);
+    return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
+  }
+
+  // Toggle Completed
+  toggleCompleted(todo: Todo): Observable<any> {
+    const url = `${this.todosUrl}/${todo.id}`;
+    return this.http.put(url, todo, httpOptions);
   }
 }
