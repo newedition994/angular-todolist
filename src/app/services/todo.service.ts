17988349db9353bf402e23589ce052442eb85331
Breyanna Vass <breyannavass@gmail.com>
2019-09-05 16:23:45 -0400

    Getting data from todo service

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,9 +1,28 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class TodoService {
+  constructor() {}
 
-  constructor() { }
+  getTodos() {
+    return [
+      {
+        id: 1,
+        title: "Todo One",
+        completed: false
+      },
+      {
+        id: 2,
+        title: "Todo Two",
+        completed: true
+      },
+      {
+        id: 3,
+        title: "Todo Three",
+        completed: false
+      }
+    ];
+  }
 }
